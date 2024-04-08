84956de4298941185bd8dc9bb12b67e6d05f9a59
sergiolima <sergiodelima22@gmail.com>
2024-04-08 20:37:24 -0300

    install express and create server

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Hello, world!');
+const express = require('express');
+
+// App
+const app = express();
+
+// Route Hello World
+app.get('/', function(request, response) {
+    response.write('Hello World!');
+    response.end();
+});
+
+// Create server
+app.listen(8080);
